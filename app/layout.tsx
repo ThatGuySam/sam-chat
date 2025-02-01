e69416917b8a4bb889fe99ad08293b5b0bd275fb
ThatGuySam <sam@sam.lc>
2025-01-31 20:29:52 -0600

    update: app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'Sam&apos;s Chat',
+  description: 'Sam&apos;s Chat is a chatbot using the AI SDK.',
 };
 
 export const viewport = {
